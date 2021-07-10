bfc93d5e766309cdf99f427c384a5d6d647decca
Daniel5629 <apxls34@gmail.com>
2021-07-10 21:47:15 +0900

    Entity - Add some entity

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -22,6 +23,12 @@ public class JpaMain {
         try {
             Order order = new Order();
             order.addOrderItem(new OrderItem());
+
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("개발자");
+
+            em.persist(book);
 
             tx.commit();
         }catch (Exception e) {
