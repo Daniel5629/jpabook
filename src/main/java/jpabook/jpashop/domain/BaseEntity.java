83d070a9082ba23f175c46058daf178279b72fa8
Daniel5629 <apxls34@gmail.com>
2021-07-10 21:53:48 +0900

    Entity - Add baseEntity

---
+++ src/main/java/jpabook/jpashop/domain/BaseEntity.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @Column(name = "last_modified_date")
+    private LocalDateTime lastModifiedDate;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+}
